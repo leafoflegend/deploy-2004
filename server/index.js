7d1bddfc8f04dabe5910462a95fd74ab1fc10431
Eliot Szwajkowski <eliotpszw@gmail.com>
2020-06-20 10:59:23 -0400

    Make sure not to nuke data and fix process env typo.

--- server/index.js
+++ server/index.js
@@ -5,9 +5,7 @@ dotenv.config();
 const { startServer, app } = require('./api/index');
 const { models, sync } = require('./db/index');
 
-const shouldForce = NODE_ENV === 'production'
-  ? false
-  : true;
+const shouldForce = process.env.NODE_ENV !== 'production';
 
 sync( shouldForce)
   .then(startServer);
